bd05cc8828c2898c57b436639f0ad0164c639a8a
Devyani-ein <99027867+Devyani-ein@users.noreply.github.com>
2022-03-11 21:06:05 +0530

    Add files via upload

---
+++ friend.cpp
@@ -0,0 +1,25 @@
+/example of C++ friend function used to print the length of a box.
+//Devyani Mahale
+
+#include <iostream>    
+using namespace std;    
+class Box    
+{    
+    private:    
+        int length;    
+    public:    
+        Box(): length(0) { }    
+        friend int printLength(Box); //friend function    
+};    
+int printLength(Box b)    
+{    
+   b.length += 10;    
+    return b.length;    
+}    
+int main()    
+{    
+    Box b;    
+    cout<<"Length of box: "<< printLength(b)<<endl;    
+    return 0;    
+}    
+
